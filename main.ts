687764575d8cc873f8e604a182606c20474ca5d7
nadiawaseem <161966596+Nadiawaseem@users.noreply.github.com>
2024-06-08 17:02:30 -0700

    Add files via upload

---
+++ main.ts
@@ -0,0 +1,30 @@
+  let car = 'subaru';
+
+  console.log("Is car == 'subaru' ? I predict True");
+
+  console.log(car == 'subaru')
+
+  console.log("Is car == 'subaru' ? I predict True");
+
+  console.log(car !== 'subaru')
+
+  console.log("Is car == 'subaru' ? I predict True");
+
+  console.log(car !== 'ford')
+
+  console.log("Is car == 'Toyota' ? I predict True");
+
+  console.log(car !== 'Toyota')
+
+  console.log("Is car == 'Toyota' ? I predict True");
+
+  console.log(car !== 'Toyota')
+  console.log("Is car == 'Toyota' ? I predict True");
+  console.log("Is car !== 'suzuki' ? I predict True");
+
+  console.log(car !== 'suzuki')
+
+
+
+
+
